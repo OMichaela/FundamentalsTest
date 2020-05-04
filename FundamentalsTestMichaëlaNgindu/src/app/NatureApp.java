28d96239ebd2267f9ea5237046d8287e603ffefa
michaelanginduwanzengu <michaelanginduwanzengu@192.168.0.167>
2020-05-04 15:32:20 +0200

    Last commit Monday

--- FundamentalsTestMichaëlaNgindu/src/app/NatureApp.java
+++ FundamentalsTestMichaëlaNgindu/src/app/NatureApp.java
@@ -1,18 +1,27 @@
 package app;
 
-import java.util.List.*;
-import java.util.Set.*;
 import plant_entities.*;
 import animal_entities.*;
 import service.*;
-import java.util.Scanner;
-import java.util.Set;
+
 
 public class NatureApp {
 
 	public static void main(String[] args) {
-		 String elm, weed, beech, raspberryBush, wisteria;
-		 String roeDeer,goat,wolf,squirrel,fox,hare,lynx, boar, hedgehog;
+		 String elm = "Elm";
+		 String weed ="Weed";
+		 String beech = "Beech";
+		 String raspberryBush="RaspberryBush";
+		 String wisteria= "Wisteria";
+		 String roeDeer= "RoeDeer";
+		 String goat= "Goat";
+		 String wolf= "Wolf";
+		 String squirrel="Squirrel";
+		 String fox="Fox";
+		 String hare="Hare";
+		 String lynx="Lynx";
+		 String boar="Boar";
+		 String hedgehog="Hedgehog";
 		
 		ForestNoteBook natureDiary = new ForestNoteBook();
 		
@@ -52,35 +61,33 @@ public class NatureApp {
 	
 	Herbivore herbivore1 = new Herbivore(roeDeer, 22, 65, 110);
 	herbivore1.addPlantToDiet(weed1);
-	herbivore1.addPlantToDiet(wisteria);
+	herbivore1.addPlantToDiet(flower);
 	natureDiary.addAnimal(herbivore1);
 	
 	Herbivore herbivore2 = new Herbivore(goat, 40, 54, 90);
 	herbivore2.addPlantToDiet(weed1);
-	herbivore2.addPlantToDiet(raspberryBush);
+	herbivore2.addPlantToDiet(bush);
 	natureDiary.addAnimal(herbivore2);
 	
 	Herbivore herbivore3 = new Herbivore(hare, 5, 53, 53);
-	herbivore3.addPlantToDiet(raspberryBush);
+	herbivore3.addPlantToDiet(bush);
 	natureDiary.addAnimal(herbivore3);
 	
 	Omnivore omnivore1 = new Omnivore(squirrel, 0.600, 21, 40);
-	omnivore1.addPlantToDiet(weed);
+	omnivore1.addPlantToDiet(weed1);
 	omnivore1.setMaxFoodSize(0.600);
 	natureDiary.addAnimal(omnivore1);
 	
 	Omnivore omnivore2 = new Omnivore(fox, 7, 45, 90);
-	omnivore2.addPlantToDiet(elm);
+	omnivore2.addPlantToDiet(tree2);
 	omnivore2.setMaxFoodSize(0.500);
 	natureDiary.addAnimal(omnivore2);
 	
 	Omnivore omnivore3 = new Omnivore(boar, 90, 110, 190);
-	omnivore3.setPlanttoDiet(beech);
+	omnivore3.addPlantToDiet(tree1);
 	omnivore3.setMaxFoodSize(1.5);
 	natureDiary.addAnimal(omnivore3);
 	
-	
-
 	
 	int AnimalCount = natureDiary.getAnimalCount();
 	
@@ -90,12 +97,10 @@ public class NatureApp {
 	int PlantCount = natureDiary.getPlantCount();
 		
 		System.out.println("Number of Plants is:"+ PlantCount);
-	
-
 			
 		natureDiary.printNotebook();	
 		
-				
+			
 			for(Carnivore c: natureDiary.getCarnivores())
 				System.out.println(c);
 			
